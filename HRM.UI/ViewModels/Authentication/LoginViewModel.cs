c6525dfdb79659195cfea407ff3865c0a47bc066
HOANG MINH HIEU <118611264+hieu202@users.noreply.github.com>
2024-04-15 13:21:59 +0700

    add authentication

---
+++ HRM.UI/ViewModels/Authentication/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.UI.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+    }
+}
