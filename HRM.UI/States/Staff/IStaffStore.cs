61be2668088f4df9f981a0f55abc1dcacb908f0e
HOANG MINH HIEU <118611264+hieu202@users.noreply.github.com>
2024-04-17 23:42:37 +0700

    update view

---
+++ HRM.UI/States/Staff/IStaffStore.cs
@@ -0,0 +1,16 @@
+﻿using HRM.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.UI.States.Staff
+{
+    public interface IStaffStore
+    {
+        NhanSu CurrentNhanSu { get; set; }
+
+        event Action StateChanged;
+    }
+}
--- HRM.UI/ViewModels/StaffCVViewModel.cs
+++ HRM.UI/ViewModels/StaffCVViewModel.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using HRM.Core.Repositories;
+using HRM.Domain.Models;
+using HRM.UI.Commands;
+using HRM.UI.Factories;
+using HRM.UI.Stores;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +16,60 @@ namespace HRM.UI.ViewModels
 {
     public class StaffCVViewModel : BaseViewModel
     {
-        public ICommand StaffCVCommand { get; set; }
+        private ObservableCollection<NhanSu> _list;
+        public ObservableCollection<NhanSu> List
+        {
+            get => _list;
+            set
+            {
+                _list = value;
+                OnPropertyChanged();
+            }
+        }
+        private NhanSu _selectedItem;
+        public NhanSu SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _filter;
+        public string Filter
+        {
+            get => _filter;
+            set
+            {
+                _filter = value;
+                OnPropertyChanged();
+                LoadData();
+            }
+        }
+        private readonly IRepository<NhanSu> _repository;
+        private readonly IViewModelFactory _viewModelFactory;
+        private readonly MainContentStore _mainContentStore;
+        public ICommand StaffViewCommand { get; set; }
+        public StaffCVViewModel(IViewModelFactory viewModelFactory, MainContentStore mainContentStore, IRepository<NhanSu> repository)
+        {
+            _repository = repository;
+            _viewModelFactory = viewModelFactory;
+            _mainContentStore = mainContentStore;
+            StaffViewCommand = new RelayCommand<object>(p => true, p =>
+            {
+                mainContentStore.CurrentViewModel = viewModelFactory.CreateViewModel(Defines.EViewTypes.ChildContent);
+            });
+            LoadData();
+
+        }
+        private void LoadData()
+        {
+            List = new ObservableCollection<NhanSu>(_repository.AsQueryable().Include(x => x.BoPhan).Include(x => x.ChucVu).Include(x => x.ViTri).ToList());
+            if (!String.IsNullOrWhiteSpace(Filter))
+            {
+                List = new ObservableCollection<NhanSu>(_repository.AsQueryable().Where(x => x.MaNhanVien.Contains(Filter) || x.HoTen.Contains(Filter)).ToList());
+            }
+        }
     }
 }
