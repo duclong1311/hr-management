1c947430199d75cd4a7a2f44b6c0d51139cbcf09
Duc Long <duclong1311.work@gmail.com>
2024-04-18 10:52:50 +0700

    Update HoSo Model

--- HRM.Domain/Models/HoSo.cs
+++ HRM.Domain/Models/HoSo.cs
@@ -1,6 +1,7 @@
 ﻿using HRM.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,18 @@ namespace HRM.Domain.Models
         public DateTime SinhNgay { get; set; }
         public string NoiSinh { get; set; }
         public string NguyenQuan { get; set; }
-        public string DanToc {  get; set; }
+        public string DanToc { get; set; }
+        public string TonGiao { get; set; }
+        public string CCCD { get; set; }
+        public DateTime CapNgay { get; set; }
+        public string NoiCap { get; set; }
+        public string TrinhDoVanHoa { get; set; }
+        public string KetNapDoan { get; set; }
+        public string NoiKetNapDoan { get; set; }
+
+        public string KetNapDang { get; set; }
+        public string NoiketNapDang {  get; set; }
+        public string KhenThuong { get; set; }  
+        public string SoThich { get; set; }
     }
 }
