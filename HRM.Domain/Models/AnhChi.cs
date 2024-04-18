e69dd8fc71cd5350617fe53edaa843be7a26df16
Duc Long <duclong1311.work@gmail.com>
2024-04-18 11:36:26 +0700

    Update binding and model

---
+++ HRM.Domain/Models/AnhChi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRM.Core.Entities;
+
+namespace HRM.Domain.Models
+{
+    public class AnhChi : IEntity
+    {
+        public string HoTenAnhChi { get; set; }
+        public DateTime NamSinh { get; set; }
+        public string NgheNghiep { get; set; }
+        public string CoQuanCongTac { get; set; }   
+    }
+}
---
+++ HRM.Domain/Models/QuaTrinhCongTac.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRM.Core.Entities;
+
+namespace HRM.Domain.Models
+{
+    public class QuaTrinhCongTac : IEntity
+    {
+        public DateTime TuNgayDenNgay { get; set; }
+        public string DonViCongTac { get; set; }
+        public string ChucVu { get; set; }
+    }
+}
