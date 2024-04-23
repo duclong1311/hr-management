d0b64fff7d55d04a7856bd4eacbe91444922c0d5
Duc Long <duclong1311.work@gmail.com>
2024-04-23 18:35:34 +0700

    Nhập công

--- HRM.Domain/Models/BangCong.cs
+++ HRM.Domain/Models/BangCong.cs
@@ -10,11 +10,14 @@ namespace HRM.Domain.Models
     public class BangCong : IEntity
     {
         public DateOnly ThangNam { get; set; }
+        public string MaNhanVien { get; set; }
+        public string HoTen {  get; set; }
         public double TongTimeOT { get; set; }
         public int TongSoNgayCong {  get; set; }
+        public int TongSoNgayCongCN {  get; set; }
+        public int TongSoNgayCongNgayLe { get; set; }
         public float DiMuonVeSom { get; set; }
         public bool ThuongChuyenCan { get; set; }   
         public int NgayNghiPhep { get; set; }
-        public int NgayKhongNghiPhep { get; set; }
     }
 }
